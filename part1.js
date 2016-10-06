4129cee4173cb78d72b782fdb21e971f908803e8
sahil-github29 <github2238>
2016-10-06 21:23:05 +0530

    New Test commit

--- part1.js
+++ part1.js
@@ -11,6 +11,6 @@ while(gotName == 45) {
   var username = prompt("what's yout name");
   if(confirm("Are you sure your name is " + username + "?")){
     alert("Sup " + username);
-    gotName = false;
+    gotName = false; 
   }
 }
