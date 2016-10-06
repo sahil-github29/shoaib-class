0fb1ae6f9496f3ca9be109d687efab75397b0717
sahil <sahil123>
2016-10-06 20:38:34 +0530

    New Test commit

---
+++ part1.js
@@ -0,0 +1,16 @@
+/*console.log("This is our first JavaScript");
+
+console.log("This" == "This");
+
+var fruit = "apple is great to have";
+
+console.log(typeof fruit)*/
+
+var gotName = 45;
+while(gotName == 45) {
+  var username = prompt("what's yout name");
+  if(confirm("Are you sure your name is " + username + "?")){
+    alert("Sup " + username);
+    gotName = false;
+  }
+}
