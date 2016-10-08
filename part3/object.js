c6783b6fe8eb193484a778f1b3f69989075d4518
sahil <sahil-github29>
2016-10-08 14:01:51 +0530

    project

--- part3/object.js
+++ part3/object.js
@@ -1,27 +1,37 @@
 var myBooks = ["Great Exception", "PHP", "JAVA"]
 
 var myBox = {
+  RedBox:["height =8cm", "width= 12cm", "length=15cm"],
   height : 8, width: 12, length : 15,
   packed : true,
-  "box content"  : ["Glasses", " Shades"],
-  Books : myBooks,
-  mylatops : {
-    language : {
-      mylanguage : ["javascript", "html"],
-      mylatops : {
-        apple : "has",
-        Lenovo : "Lenovo is a good laptop"
+  content  : ["Glasses", " Shades"],
+
+BooksBox:{
+  mylanguage : {C:["basic","loops","string","functions"],
+  javaaa:["javaBasic","javaLoops"],
+  JavaScript:["JavaScriptBasic","basicLoop","GIT"],
+  csharp:["basicC#","OOPS","functionsC#","SQL"],
+
+  TypesofBooks : {
+    ProgrammingLanguage : ["c","java","JavaScript","c#"],
+    completeReference: "Programming best book ever"
+
       }
     }
   }
 }
-console.log(myBox.mylatops.language.mylanguage);
+
+//for (var i = 0; i < myBox.BooksBox.mylanguage.csharp.length; i++) {
+
+// console.log(myBox.BooksBox.mylanguage.cs[i]);
+//}
+//console.log(myBox.height);
 //console.log(myBox['length']);
 //console.log(myBox.length);
 //console.log(myBox.packed);
 //console.log(myBox.content[1]);
 //console.log(myBox.Books[0]);
-myBox.Books[0] = "MongoDB";
-myBox.shipped = "success";
+//myBox.Books[0] = "MongoDB";
+//myBox.shipped = "success";
 
-console.log(myBox);
+console.log(myBox.BooksBox.mylanguage.TypesofBooks.completeReference);
