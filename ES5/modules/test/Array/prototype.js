73d9c35a5c2c231d59eac02221440837742047f2
Shohaib khan <shohaibkhan1@gmail.com>
2016-11-09 18:28:33 +0530

    new

--- ES5/modules/test/Array/prototype.js
+++ ES5/modules/test/Array/prototype.js
@@ -6,13 +6,13 @@ The prototype property allows you to add properties and methods to any object
 */
 
 
- function book(title,author,store) {
+ function Book(title,author,store) {
       this.title = title;
       this.store = store;
       this.author = author;
  }
-   var myBook = new book("Perl", "Mohtashim","kyala");
-   book.prototype.name = null; //add new properties help of prototype
+   var myBook = new Book("Perl", "Mohtashim","kyala");
+   bugook.prototype.name = null; //add new properties help of prototype
    book.prototype.price = null;
    myBook.price = 100;
    myBook.name = 'shohaib';
