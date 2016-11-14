46ef2f4c899ce1a6e3f094dda35aa9cc6e6536ad
msahil <msahil@juniper.net>
2016-11-14 11:01:00 +0530

    Stated ES6

---
+++ ES6/app.js
@@ -0,0 +1,11 @@
+var http = require('http')
+var useLet = require('./let/let.js');
+
+http.createServer(function(req, res) {
+  res.write("We are using ES6")
+  // use of let
+  console.log(useLet("test"))
+
+  res.end()
+}).listen(8080)
+console.log('listening on port 8080');
---
+++ ES6/let/let.js
@@ -0,0 +1,10 @@
+"use strict"
+
+var useLet = function(flag) {
+
+  if (flag) {
+    var x = 3
+  }
+  return x
+}
+module.exports = useLet
